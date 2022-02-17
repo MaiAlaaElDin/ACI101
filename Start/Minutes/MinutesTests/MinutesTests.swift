01bae223a849fb32681b11dc8131b87ec3b99488
Mai Alaa El Din <mai.alaa.elsaeed@gmail.com>
2022-02-17 15:46:17 +0200

    Unit testing

---
+++ Start/Minutes/MinutesTests/MinutesTests.swift
@@ -0,0 +1,34 @@
+//
+//  MinutesTests.swift
+//  MinutesTests
+//
+//  Created by Mai Alaa on 17/02/2022.
+//  Copyright © 2022 Microsoft. All rights reserved.
+//
+
+import XCTest
+@testable import Minutes
+
+class MinutesTests: XCTestCase {
+
+    var entryUnderTesting: Entry!
+    
+    override func setUp() {
+        super.setUp()
+        entryUnderTesting = Entry("Title", "Content")
+    }
+    
+    override func tearDown() {
+        entryUnderTesting = nil
+        super.tearDown()
+    }
+    
+    func testSerialization() {
+        let serializedData = FileEntryStore.serialize(entryUnderTesting)
+        let deseralizedData = FileEntryStore.deserialize(serializedData!)
+        
+        XCTAssertEqual(deseralizedData?.title ,"Title", "Titles don't match")
+        XCTAssertEqual(deseralizedData?.content ,"Content", "Contents don't match")
+    }
+
+}
